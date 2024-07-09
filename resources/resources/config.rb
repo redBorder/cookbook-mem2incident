ae3c4d3b18b1428dfc7f3504430b41e4441f1c7e
Miguel Negron <manegron@redborder.com>
2024-07-09 14:25:42 +0100

    Fix kind of insecure skip verify resource

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -10,6 +10,6 @@ attribute :cdomain, kind_of: String, default: 'redborder.cluster'
 # redborder-mem2incident config.yml
 attribute :memcached_servers, kind_of: Array, default: ['localhost:11211']
 attribute :api_endpoint, kind_of: String, default: 'https://webui.service/api/v1/incidents'
-attribute :insecure_skip_verify, kind_of: Boolean, default: true
+attribute :insecure_skip_verify, kind_of: [TrueClass, FalseClass], default: true
 attribute :loop_interval, kind_of: Integer, default: 60
 attribute :auth_token, kind_of: String, default: 'your_auth_token_here'
